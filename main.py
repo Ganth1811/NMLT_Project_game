e58d39b179e68b88dbe38c73a50158239d8bb8b6
Ganth1811 <quangthangngo181@gmail.com>
2023-12-04 08:48:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ while 1:
     #Getting the events and passing it to the current state so it can procress the events accordingly
     events = pygame.event.get()
 
-    #Getting the next state of the game
+    #Getting the next state of the game 
     next_state = current_state.processEvent(events)
     #if there is a next state switch to it
     if next_state is not None:
